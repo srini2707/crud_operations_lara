5adfacee6f7ae7c24a44999e2f0c08270d344069
srini2707 <a20cadb37@sjctnc.edu.in>
2023-11-22 21:55:10 +0530

    file uploaded for firts time

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Models\Post;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $posts = Post::all();
+    return view('posts.index', compact('posts'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title' => 'required|max:255',
+            'body' => 'required',
+          ]);
+        Post::create($request->all());
+    return redirect()->route('posts.index')
+      ->with('success', 'Post created successfully.');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $post = Post::find($id);
+    return view('posts.show', compact('post'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $request->validate([
+            'title' => 'required|max:255',
+            'body' => 'required',
+          ]);
+          
+        $post = Post::find($id);
+    $post->update($request->all());
+    return redirect()->route('posts.index')
+      ->with('success', 'Post updated successfully.');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $post = Post::find($id);
+    $post->delete();
+    return redirect()->route('posts.index')
+      ->with('success', 'Post deleted successfully');
+    }
+
+    public function edit($id)
+  {
+    $post = Post::find($id);
+    return view('posts.edit', compact('post'));
+  }
+
+  public function create()
+  {
+    return view('posts.create');
+  }
+}
